7e0cd0971cf1e03ff29be81483a9eefb657f7e56
anshu1016 <arunshukla98710@gmail.com>
2025-09-14 16:09:48 +0530

    Advance Retrieval Implementation Done

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -5,6 +5,15 @@ from langchain_core.documents import Document
 from dotenv import load_dotenv
 from utils.model_loader import ModelLoader
 from utils.config_loader import load_config
+import sys
+from pathlib import Path
+from langchain.retrievers.document_compressors import LLMChainFilter
+from langchain.retrievers import ContextualCompressionRetriever
+
+
+# Add the project root to the Python path for direct script execution
+project_root = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(project_root))
 
 
 class Retriever:
@@ -52,9 +61,27 @@ class Retriever:
 
         if not self.retriever:
             top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
-            retriever = self.vstore.as_retriever(search_type="similarity", search_kwargs={"k": top_k})  
-            print("Retriever loaded successfully")
-            return retriever
+            # Use Maximal Marginal Relevance (MMR) for diverse results
+            # Compute Maximal Marginal Relevance (MMR). MMR is a technique used to select documents that are both relevant to the query and diverse among themselves. This function returns the indices of the top-k embeddings that maximize the marginal relevance.
+            mmr_retriever=self.vstore.as_retriever(
+                search_type="mmr",
+                search_kwargs={"k": top_k,
+                                "fetch_k": 20,
+                                "lambda_mult": 0.7,
+                                "score_threshold": 0.3
+                               })
+            print("Retriever loaded successfully.")
+            
+            llm = self.model_loader.load_llm()
+            
+            compressor=LLMChainFilter.from_llm(llm)
+            
+            self.retriever = ContextualCompressionRetriever(
+                base_compressor=compressor, 
+                base_retriever=mmr_retriever
+            )
+            
+            return self.retriever
 
     def call_retriever(self,user_query):
         """
